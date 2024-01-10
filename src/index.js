40f438a78c69f1c74a835f72b2a80ec79fc48dd8
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-01-10 23:30:32 +0200

    Add API integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function setWeather(response) {
+  let temperatureElement = document.querySelector("#temp-today");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector(".city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "78d37e60cbae34922894o08be20fe53t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(setWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-input");
@@ -7,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector(".search");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Pretoria");
