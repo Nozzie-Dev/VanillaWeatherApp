2f41efaa4e07045819bf55e541b0b2c74404601b
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-01-13 14:33:48 +0200

    Add all weather details from API

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ function setWeather(response) {
   let temperatureElement = document.querySelector("#temp-today");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector(".city");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let icon = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  time.innerHTML = setDate(date);
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+
+function setDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
